5b29a60b4297e834aa0643d090882d16a7c0057b
ailedt <deliatreichler@gmail.com>
2016-06-23 18:46:47 +0200

    remove listener, move configs to yml

--- src/main/java/mtg/priceUpdater/MtgPriceUpdaterApplication.java
+++ src/main/java/mtg/priceUpdater/MtgPriceUpdaterApplication.java
@@ -1,27 +1,14 @@
 package mtg.priceUpdater;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.boot.actuate.system.ApplicationPidFileWriter;
-
-import javax.annotation.PreDestroy;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class MtgPriceUpdaterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication notificationMicroService = new SpringApplication(MtgPriceUpdaterApplication.class);
-		notificationMicroService.addListeners(new ApplicationPidFileWriter("notification-micro-service.pid"));
-		notificationMicroService.run(args);
+		new SpringApplication(MtgPriceUpdaterApplication.class).run(args);
 	}
 }
