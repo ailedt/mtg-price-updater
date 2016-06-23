5d004e15c2aa015a26a593fbcd282a26e6d03acb
ailedt <deliatreichler@gmail.com>
2016-06-23 12:16:19 +0200

    introduce eureka

--- src/main/java/mtg/priceUpdater/MtgPriceUpdaterApplication.java
+++ src/main/java/mtg/priceUpdater/MtgPriceUpdaterApplication.java
@@ -7,13 +7,16 @@ import org.springframework.amqp.core.TopicExchange;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.boot.actuate.system.ApplicationPidFileWriter;
 
 import javax.annotation.PreDestroy;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MtgPriceUpdaterApplication {
 
 	public final static String QUEUE = "mtg-price-update";
@@ -37,7 +40,9 @@ public class MtgPriceUpdaterApplication {
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(MtgPriceUpdaterApplication.class, args);
+		SpringApplication notificationMicroService = new SpringApplication(MtgPriceUpdaterApplication.class);
+		notificationMicroService.addListeners(new ApplicationPidFileWriter("notification-micro-service.pid"));
+		notificationMicroService.run(args);
 	}
 
 	@PreDestroy
